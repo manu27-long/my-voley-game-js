ba55c5c709a594e7c0c8b3bb375104199067b30d
manu27-long <manuelaizidorio.2019.2@gmail.com>
2021-08-20 14:44:27 +0000

    alguns test

--- set.test.js
+++ set.test.js
@@ -1,9 +1,29 @@
 
 const Jogo = require('./jogo');
 
-  test('Teste', () => {
-  var novoJogo = new Jogo();
+  test('Testar data', () => {
+  novoJogo = new Jogo('08-09-2021', '08:00', 'Rio Largo', 'Luiz');
 
-  expect(novoJogo.getData()).toEqual('12-09-2019');
+  expect(novoJogo.getData()).toBe('08-09-2021');
 }
   );
+
+  test('testar mudar a data', () => {
+   var novoJogo = new Jogo('08-09-2021', '08;00', 'Rio Largo', 'Luiz')
+   novoJogo.setData('20-09-2021');
+  expect(novoJogo.getData()).toBe('20-09-2021');
+  }
+   );
+
+   
+   beforeAll(() =>{
+     console.log('Before all');
+   }
+    );
+
+    beforeEach(() => {
+      console.log('Before each');
+    }
+     );
+
+     
