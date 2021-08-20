ed44622c532db924cffb1cca0cee005805a320fa
manu27-long <manuelaizidorio.2019.2@gmail.com>
2021-08-20 14:49:56 +0000

    alteracoes no test

--- set.test.js
+++ set.test.js
@@ -3,7 +3,6 @@ const Jogo = require('./jogo');
 
   test('Testar data', () => {
   novoJogo = new Jogo('08-09-2021', '08:00', 'Rio Largo', 'Luiz');
-
   expect(novoJogo.getData()).toBe('08-09-2021');
 }
   );
@@ -11,7 +10,7 @@ const Jogo = require('./jogo');
   test('testar mudar a data', () => {
    var novoJogo = new Jogo('08-09-2021', '08;00', 'Rio Largo', 'Luiz')
    novoJogo.setData('20-09-2021');
-  expect(novoJogo.getData()).toBe('20-09-2021');
+   expect(novoJogo.getData()).toBe('20-09-2021');
   }
    );
 
